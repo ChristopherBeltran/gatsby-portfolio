2414746a86ef554da48909f76b77a2705163793b
Christopher Beltran <christopherbeltran@Christophers-MacBook-Pro-2.local>
2020-03-22 12:18:06 -0600

    Removing contact form and adding a gmail mailto link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
+    title: 'Gatsby Portfolio',
+    author: 'Christopher Beltran',
     description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
   },
   plugins: [
--- src/components/Main.js
+++ src/components/Main.js
@@ -98,28 +98,6 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
           <ul className="icons">
             <li>
               <a
@@ -135,6 +113,14 @@ class Main extends React.Component {
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:christod.beltran@gmail.com"
+                className="icon fa-google"
+              >
+                <span className="label">GMail</span>
               </a>
             </li>
           </ul>
