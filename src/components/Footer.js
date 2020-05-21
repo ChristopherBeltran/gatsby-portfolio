6e5d0482668c67e8c544594dc22cb08f5c390067
Christopher Beltran <christod.beltran@gmail.com>
2020-05-21 14:28:56 -0600

    Styling fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Footer = (props) => ( <
-    footer id = "footer"
-    style = {
-        props.timeout ? {
-            display: 'none'
-        } : {}
-    } >
-    <
-    p className = "copyright" > & copy; Christopher Beltran 2020 developed with Gatsby Starter - Dimension < /p> <
-    /footer>
+const Footer = props => (
+  <footer
+    id="footer"
+    style={
+      props.timeout
+        ? {
+            display: 'none',
+          }
+        : {}
+    }
+  >
+    <p className="copyright">
+      {' '}
+      & copy; Christopher Beltran 2020 developed with Gatsby Starter - Dimension{' '}
+    </p>{' '}
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: PropTypes.bool
+  timeout: PropTypes.bool,
 }
 
 export default Footer
