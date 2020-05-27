6cb05fbb16431d88e35100167682423fac36c6d5
Christopher Beltran <christod.beltran@gmail.com>
2020-05-27 11:09:26 -0600

    Footer copy fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = props => (
-  <footer
-    id="footer"
-    style={
-      props.timeout
-        ? {
-            display: 'none',
-          }
-        : {}
-    }
-  >
-    <p className="copyright">
-      {' '}
-      & copy; Christopher Beltran 2020 developed with Gatsby Starter - Dimension{' '}
-    </p>{' '}
-  </footer>
+    <footer id="footer"
+        style={
+            props.timeout ? {
+                display: 'none'
+            } : {}
+    }>
+        <p className="copyright">
+            {' '}
+            Christopher Beltran 2020 developed with Gatsby Starter - Dimension{' '} </p>
+        {' '} </footer>
 )
 
 Footer.propTypes = {
-  timeout: PropTypes.bool,
+    timeout: PropTypes.bool
 }
 
 export default Footer
